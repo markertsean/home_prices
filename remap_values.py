de3e955e6fec42f8800c654772427bc3be72fcfd
Sean Markert <markertsean0@gmail.com>
2017-04-14 19:15:52 -0600

    Added regression optimizer which works pretty fucking well

--- remap_values.py
+++ remap_values.py
@@ -1,6 +1,13 @@
 import numpy  as np
 import pandas as pd
 
+from sklearn.utils import shuffle
+from sklearn.model_selection import KFold
+from sklearn.metrics import r2_score
+from sklearn.svm import SVR
+from sklearn.decomposition import PCA
+from sklearn.model_selection import GridSearchCV
+from sklearn.base import clone
 
 area_classification_list = [ 'MSSubClass', 'MSZoning', 'Neighborhood', 'Condition1', 'Condition2', 'MoSold', 
                              'YrSold', 'SaleType', 'SaleCondition' ] 
@@ -37,10 +44,12 @@ bldg_map     = { '1Fam':0, '2FmCon':1, '2fmCon':1, 'Duplex':2, 'TwnhsE':3, 'Twnh
 style_map    = { '1Story':0, '1.5Fin':1, '1.5Unf':2, '2Story':3, '2.5Unf':4, '2.5Fin':5 ,'SFoyer':6, 'SLvl':7 }
 roofSty_map  = { 'Flat':0, 'Gable':1, 'Gambrel':2, 'Hip':3, 'Mansard':4, 'Shed':5 }
 roofMat_map  = { 'ClyTile':0, 'CompShg':0, 'Membran':0, 'Metal':0, 'Roll':0, 'Tar&Grv':1, 'WdShake':2, 'WdShngl':3 }
-exter_map    = { 'AsbShng':0, 'AsphShn':1, 'BrkComm':2,	'Brk Cmn':2, 'BrkFace':3, 'CBlock':4, 'CemntBd':5, 'CmentBd':5, 'HdBoard':6, 'ImStucc':7,	
-                 'MetalSd':8, 'Other':9, 'Plywood':10,  'PreCast':11,  'Stone':12, 'Stucco':13, 'VinylSd':14, 'Wd Sdng':15, 
-                 'WdShing':16, 'Wd Shng':16 }
-masn_map     = { 'None':0, 'BrkCmn':1, 'BrkFace':2, 'CBlock':3, 'Stone':4 }
+
+exter_map    = { 'AsbShng':0, 'AsphShn':0, 'BrkComm':0,	'Brk Cmn':0, 'BrkFace':0, 'CBlock':0, 'CemntBd':0, 'CmentBd':0, 'HdBoard':2, 'ImStucc':0,	
+                 'MetalSd':3, 'Other':0, 'Plywood':1,  'PreCast':0,  'Stone':0, 'Stucco':0, 'VinylSd':0, 'Wd Sdng':4, 
+                 'WdShing':0, 'Wd Shng':0 }
+
+masn_map     = { 'None':0, 'BrkCmn':0, 'BrkFace':1, 'CBlock':0, 'Stone':2 }
 #'ExterQual', 'ExterCond', 'BsmtQual', 'BsmtCond', 'HeatingQC', 'KitchenQual', 'GarageQual', 'GarageCond', 'PoolQC'
 qual_map     = { 'NA':0, 'nan':0, 'NaN':0, 'Po':1, 'Fa':2, 'TA':3, 'Gd':4, 'Ex':5 } 
 found_map    = { 'BrkTil':0, 'CBlock':1, 'PConc':2, 'Slab':3, 'Stone':4, 'Wood':5 }
@@ -79,11 +88,14 @@ def remap_quality( quality_df ):
     # Drop BsmtFinType2
     # Drop GarageCond
     
+    foo['ExterQual']   = foo['ExterQual'].replace( {1:0, 2:0, 3:0, 4:1, 5:1 } )
+    foo['GaragerQual'] = foo['GarageQual'].replace( {2:0, 0:1, 3:2, 4:2, 5:2} )
+    
     foo['BsmtQual']    = quality_df['BsmtQual']    .replace(   bsmtquall ).fillna( 0 )
     foo['ExterCond']   = quality_df['ExterCond']   .replace(  externcond )
     foo['HeatingQC']   = quality_df['HeatingQC']   .replace(  externcond )
     #foo['RoofStyle']   = quality_df['RoofStyle']   .replace( roofSty_map )
-    foo['RoofMatl']    = quality_df['RoofMatl']    .replace( roofMat_map )
+    #foo['RoofMatl']    = quality_df['RoofMatl']    .replace( roofMat_map )
     foo['Exterior']    = quality_df['Exterior1st'] .replace(   exter_map )
     #foo['Exterior2nd'] = quality_df['Exterior2nd'] .replace(   exter_map )
     foo['MasVnrType']  = quality_df['MasVnrType']  .replace(    masn_map ).fillna( 0 )
@@ -92,15 +104,15 @@ def remap_quality( quality_df ):
     #foo['BsmtFinType2']= quality_df['BsmtFinType2'].replace(  basFin_map ).fillna( 0 )
     foo['Electrical']  = quality_df['Electrical']  .replace(    elec_map ).fillna( 0 )
     foo['GarageFinish']= quality_df['GarageFinish'].replace(    garF_map ).fillna( 0 )
-    foo['Fence']       = quality_df['Fence']       .replace(   fence_map ).fillna( 0 )
+    foo['Fence']       = quality_df['Fence']       .replace(   fence_map ).fillna( 0 )#.replace( {1:0, 2:1, 3:2, 4:3} )
     
-    return foo[[ 'Exterior', 'ExterQual', 'MasVnrType', 'RoofMatl', 'Fence', 
+    return foo[[ 'Exterior', 'ExterQual', 'MasVnrType', 'Fence', 
                  'Bsmt', 'BsmtQual', 'GarageQual', 'GarageFinish',
                  'FireplaceQu', 'HeatingQC', 'KitchenQual', 'Electrical']].astype(int)
 
 # Remap all the home list stuff to integers
 def remap_home( quality_df ):
-    bigList = [ 'KitchenAbvGr', 'TotRmsAbvGrd', 'Fireplaces', 'GarageArea' ]
+    bigList = [ 'KitchenAbvGr', 'TotRmsAbvGrd', 'Fireplaces' ]
 
     # Make total SF from 1st&2nd Floor
     # Make column multistory
@@ -119,10 +131,10 @@ def remap_home( quality_df ):
     foo = quality_df[ bigList ].copy()
     
     #foo['InsideSF']   = quality_df['1stFlrSF']     + quality_df['2ndFlrSF']
-    foo['InsideSF']   = quality_df['GrLivArea']
-    foo['OutsideSF']  = quality_df['WoodDeckSF']   + quality_df['OpenPorchSF'] +\
+    foo['InsideSF']   = quality_df['GrLivArea'].astype(float)
+    foo['OutsideSF']  =(quality_df['WoodDeckSF']   + quality_df['OpenPorchSF'] +\
                         quality_df['EnclosedPorch']+ quality_df['3SsnPorch']   +\
-                        quality_df['ScreenPorch']
+                        quality_df['ScreenPorch']).astype(float)
         
     foo['MultiStory'] =(quality_df['2ndFlrSF']>0 ).astype(int)
     
@@ -130,11 +142,12 @@ def remap_home( quality_df ):
                         quality_df['FullBath']     + quality_df['HalfBath']
     
     #foo['Utilities']  = quality_df['Utilities']  .replace(  util_map )
-    foo['BldgType']   = quality_df['BldgType']   .replace(  bldg_map )
-    foo['HouseStyle'] = quality_df['HouseStyle'] .replace( style_map )
+    foo['BldgType']   = quality_df['BldgType']   .replace(  bldg_map )#.replace( {1:3, 3:1} )
+    foo['HouseStyle'] = quality_df['HouseStyle'] .replace( style_map )#.replace( {3:0, 0:1, 1:2, 2:1, 4:1, 5:1, 6:3, 7:4})
     #foo['Heating']    = quality_df['Heating']    .replace(  heat_map )
     foo['CentralAir'] = quality_df['CentralAir'] .replace( yesno_map )
-    foo['GarageType'] = quality_df['GarageType'] .replace(  garT_map ).fillna(0)
+    foo['GarageType'] = quality_df['GarageType'] .replace(  garT_map ).fillna(0).replace({ 4:1, 1:2, 5:4, 6:4 }).astype(int)
+    foo['GarageArea'] = quality_df['GarageArea']                      .fillna(0).astype(float)
     #foo['MiscFeature']= quality_df['MiscFeature'].replace(  feat_map ).fillna( 0 )
     
     foo[ foo['KitchenAbvGr']==0 ] = 1
@@ -147,8 +160,8 @@ def remap_area( inp_df ):
                 'SaleType', 'SaleCondition' ]
 
     foo = inp_df[ ['YrSold','MSSubClass'] ].copy()
-    foo['MSSubClass']    = inp_df['MSSubClass']   .replace(    MSSub_map )
-    foo['MSZoning']      = inp_df['MSZoning']     .replace( MSZoning_map )
+    foo['MSSubClass']    = inp_df['MSSubClass']   .replace(    MSSub_map )#.replace({0:0, 5:1, 11:2, 6:3, 4:4, 10:5, 1:10, 2:7, 3:0, 7:6, 8:8, 9:9, 12:0, 13:0, 14:0, 15:0 })
+    foo['MSZoning']      = inp_df['MSZoning']     .replace( MSZoning_map )#.replace({0:0, 3:0, 5:0, 6:0, 8:0, 7:1, 1:2, 4:0, 2:3})
     foo['Neighborhood']  = inp_df['Neighborhood'] .replace(    neigh_map )
     foo['Condition1']    = inp_df['Condition1']   .replace(     cond_map )
     foo['Condition2']    = inp_df['Condition2']   .replace(     cond_map )
@@ -164,7 +177,7 @@ def remap_road( inp_df ):
     bigList = [ 'LotFrontage', 'Alley', 'PavedDrive' ]
 
     foo = inp_df[ bigList ].replace( road_map ).copy()
-    foo['Alley'     ] = inp_df['Alley'     ].replace(  road_map )
+    foo['Alley'     ] = inp_df['Alley'     ].replace(  road_map ).fillna(0).astype(int)
 #    foo['Street'    ] = inp_df['Street'    ].replace(  road_map )
     foo['PavedDrive'] = inp_df['PavedDrive'].replace( yesno_map )
    
@@ -183,3 +196,212 @@ def remap_land( inp_df ):
     foo['LotArea'  ]   = np.log10( foo['LotArea'] )
     
     return foo[bigList]
+
+
+# Normalize index
+def normalize_column( inp_df, column, maxVal=None, minVal=None ):
+    
+    new_column = inp_df.copy()
+    
+    max_value = maxVal
+    min_value = minVal
+    
+    if( max_value == None ):
+        max_value = inp_df[column].max()
+    if( min_value == None ):
+        min_value = inp_df[column].min()
+        
+    new_column[column] = ( inp_df[ column ] - float(min_value) ) / ( max_value - min_value )
+    new_column.ix[ new_column[column]<0, column ] = 0.0
+    new_column.ix[ new_column[column]>1, column ] = 1.0
+
+    return new_column[column]
+
+# Find normalization values within input sigma, returns normalization parameters that ignores outliers
+def normalize_column_sigma( inp_df, column, lower_bound=True, upper_bound=True, n_sigma=3.0 ):
+    
+    new_column = inp_df[column].copy()
+    
+    myMean = new_column.mean()
+    myStd  = new_column.std()
+    
+    if ( lower_bound ):
+        new_column =  new_column[ new_column > ( myMean - n_sigma * myStd ) ]
+        
+    if ( upper_bound ):
+        new_column =  new_column[ new_column < ( myMean + n_sigma * myStd ) ]
+        
+    return normalize_column( inp_df, column, minVal=new_column.min(), maxVal=new_column.max())
+
+# Perform normalization on categories and areas
+def normalize_homes( inp_df ):
+    
+    my_df = inp_df.drop( ['InsideSF', 'OutsideSF', 'GarageArea', 'LotFrontage', 'LotArea'], axis=1 )
+    
+    # Normalize categories from 0 to 1
+    for col in my_df.columns.values:
+        my_df[col] = normalize_column( my_df, col )
+    
+    # Inside top and bot, log first
+    # Outside top and bot log first
+    # Garage upper
+    # Frontage upper
+    # Are upper and lower
+    my_df['InsideSF']    = np.log10( inp_df['InsideSF']+1  )
+    my_df['OutsideSF']   = np.log10( inp_df['OutsideSF']+1 )
+
+    my_df['InsideSF']    = normalize_column_sigma( my_df, 'InsideSF'  )
+    my_df['OutsideSF']   = normalize_column_sigma( my_df, 'OutsideSF' )
+
+    my_df['LotArea']     = normalize_column_sigma( inp_df, 'LotArea'  )
+    my_df['LotFrontage'] = normalize_column_sigma( inp_df, 'LotFrontage', lower_bound=False )
+    my_df['GarageArea']  = normalize_column_sigma( inp_df, 'GarageArea' , lower_bound=False )
+
+    return my_df
+
+def run_kfold( clf, train_x_df, train_y_df, nf=10 ):
+    
+    kf = KFold( n_splits=nf )
+    kf.get_n_splits( train_x_df )
+    
+    outcomes = []
+    fold = 0
+    
+    # Generate indexes for training and testing data
+    for train_index, test_index in kf.split( train_x_df ):
+        
+        fold += 1
+        
+        # Generate training and testing data from input sets
+        x_train = train_x_df.iloc[train_index]
+        y_train = train_y_df.iloc[train_index]
+        x_test  = train_x_df.iloc[test_index]
+        y_test  = train_y_df.iloc[test_index]
+        
+        
+        # Foooooooo
+        clf.fit( x_train, y_train )
+        predictions = clf.predict( x_test )
+        accuracy = r2_score( y_test, predictions)
+        outcomes.append(accuracy)
+        print("Fold {0} accuracy: {1}".format(fold, accuracy))
+        
+    mean_outcome = np.mean(outcomes)
+    print("Mean Accuracy: {0} +/- {1}".format(mean_outcome,np.std(outcomes)))
+
+def run_kfold_arr( clf, train_x_df, train_y_df, nf=10 ):
+    
+    kf = KFold( n_splits=nf )
+    kf.get_n_splits( train_x_df )
+    
+    outcomes = []
+    fold = 0
+    
+    # Generate indexes for training and testing data
+    for train_index, test_index in kf.split( train_x_df ):
+        
+        fold += 1
+        
+        # Generate training and testing data from input sets
+        x_train = train_x_df[train_index]
+        y_train = train_y_df.iloc[train_index]
+        x_test  = train_x_df[test_index]
+        y_test  = train_y_df.iloc[test_index]
+        
+        
+        # Foooooooo
+        clf.fit( x_train, y_train )
+        predictions = clf.predict( x_test )
+        accuracy = r2_score( y_test, predictions)
+        outcomes.append(accuracy)
+        print("Fold {0} accuracy: {1}".format(fold, accuracy))
+        
+    mean_outcome = np.mean(outcomes)
+    print("Mean Accuracy: {0} +/- {1}".format(mean_outcome,np.std(outcomes)))
+    
+def optimize_fit( clf, train_x, train_y, grid_params, nf=10, verbose=True ):
+    
+    kf = KFold( n_splits=nf )
+    kf.get_n_splits( train_x )
+    
+    outcomes = []
+    clf_list = []
+    fold = 0
+    
+    # Generate indexes for training and testing data
+    for train_index, test_index in kf.split( train_x ):
+        
+        fold += 1
+        
+        # Generate training and testing data from input sets
+        x_train = train_x[train_index]
+        y_train = train_y[train_index]
+        x_test  = train_x[test_index]
+        y_test  = train_y[test_index]
+        
+        
+        # 
+        
+        new_clf = GridSearchCV( clf, grid_params ) 
+        
+        new_clf.fit( x_train, y_train )
+        predictions = new_clf.predict( x_test )
+        accuracy = r2_score( y_test, predictions )
+        outcomes.append(accuracy)
+        
+        clf_list.append( clone( new_clf.best_estimator_ ) )
+        if ( verbose ):
+#            print("Fold {0} accuracy: {1}".format(fold, accuracy)), ', ', new_clf.best_score_, new_clf.best_params_
+            print "Fold %2i accuracy: %10.8f " % (fold, accuracy), ', ', '%10.8f '%new_clf.best_score_, new_clf.best_params_
+        
+    best_clf_score = 0
+    best_clf_index = 0
+    best_clf_acc   = 0
+    
+    clf_num = 0
+
+    print ' '
+
+    # Check each winning CLF against the group
+    # and pick the best of the best
+    for test_clf in clf_list:
+        
+        accuracies = []
+        fold       = 0
+        
+        for train_index, test_index in kf.split( train_x ):
+        
+            fold += 1
+
+            # Generate training and testing data from input sets
+            x_train = train_x[train_index]
+            y_train = train_y[train_index]
+            x_test  = train_x[test_index]
+            y_test  = train_y[test_index]
+        
+            test_clf.fit( x_train, y_train )
+
+            predictions = test_clf.predict( x_test )
+            accuracy = r2_score( y_test, predictions )
+            accuracies.append(accuracy)
+        
+        mean_outcome = np.mean( accuracies )
+        print "Clf %2i Mean Accuracy: %10.8f +/- %10.8f" % (clf_num,mean_outcome,np.std(accuracies))
+
+        # Figure out which clf is the best
+        if ( mean_outcome > best_clf_score ):
+            best_clf_index = clf_num
+            best_clf_score = mean_outcome
+            
+        clf_num = clf_num + 1
+    
+    # Fit and return best fit
+    ret_clf = clf_list[ best_clf_index ]
+    ret_clf.fit( train_x, train_y )
+    
+    if ( verbose ):
+        print 'Using CLF with accuracy: %10.6f' % best_clf_score
+        print 'CLF params: ', ret_clf.get_params( deep=False )
+    
+    
+    return ret_clf
